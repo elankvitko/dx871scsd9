8f7832ba26cab869f5810876baf99ad45188919c
Elan Kvitko <elan@idknyc.com>
2016-12-20 09:47:57 -0500

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161218174930) do
+ActiveRecord::Schema.define(version: 20161220143700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,8 +20,9 @@ ActiveRecord::Schema.define(version: 20161218174930) do
     t.string   "name"
     t.string   "model_number"
     t.integer  "order_id"
-    t.datetime "created_at",   null: false
-    t.datetime "updated_at",   null: false
+    t.datetime "created_at",                         null: false
+    t.datetime "updated_at",                         null: false
+    t.string   "status",       default: "Requested", null: false
     t.index ["order_id"], name: "index_items_on_order_id", using: :btree
   end
 
